fb0670da9fc7c4252762fc8a4a9deecfded55c9d
ammarelhinchi <66500132+ammarelhinchi@users.noreply.github.com>
2025-09-21 02:17:05 +0100

    exercice1.py

---
+++ exercice1.py
@@ -0,0 +1,25 @@
+# exercice1.py
+
+def afficher_par_ligne(matrice):
+    """
+    Affiche la matrice ligne par ligne.
+    matrice : liste de listes 9x9
+    """
+    # TODO: compléter
+    pass
+
+def afficher_par_colonne(matrice):
+    """
+    Affiche la matrice colonne par colonne.
+    matrice : liste de listes 9x9
+    """
+    # TODO: compléter
+    pass
+
+def afficher_par_bloc(matrice):
+    """
+    Affiche la matrice par bloc de 9 cases.
+    matrice : liste de listes 9x9
+    """
+    # TODO: compléter
+    pass
